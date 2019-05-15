7fd958a75d759575c9dcaba1f9c868e0069c73f6
amritabasu.mca09@gmail.com <amritabasu.mca09@gmail.com>
2019-05-15 20:34:35 +0200

    added counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0,
-        name: "IT"
+        count: 1,
+        name: "IT",
+        tags: ["tag1","tag2","tag3"]
+        // imageUrl: "https://picsum.photos/id/909/200/200"
     };
+
+    // constructor() {
+    //    super();
+    //    this.handleIncrement=this.handleIncrement.bind(this);
+    //    console.log("Constructor",this);
+
+    // }
+
+    style = {
+        fontSize: 10,
+        fontWeight: "bold"
+    };
+
     render() { 
-        return <div><span>{this.formatCount()}</span><button>Increment</button></div>;
+        return (
+        <div>
+            {/* <img src={this.state.imageUrl} alt="text"></img> */}
+            <span style={this.style} className={this.getBadgeClasses()}>
+            {this.formatCount()}</span>
+            <button onClick={this.handleIncrement} style={this.style} className="btn">Increment</button>
+            {/* <ul>{this.state.tags.map(x => <li>{x}</li>)}  </ul>; */}
+            {/* <span style={this.style}>{this.state.tags.map(x => <h1>{x}</h1>)} </span>; */}
+            {this.redenderTags()};
+        </div>);
     }
     formatCount() {
         const {count} = this.state;
         const {name} = this.state;
-        return count === 0 ? "Zero" + name : count;
+        console.log("in"+count);
+        var cnt = count === 0 ? "Zero" + name  : count;
+        return <h1>{cnt}</h1>;
+        
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count == 0 ? "primary" : "warning";
+        return classes;
+    }
+    redenderTags() {
+        if (this.state.tags.length == 0) return <h3>No tags</h3>;
+
+        return <span style={this.style}>{this.state.tags.map(x => <h1>{x}</h1>)} </span>;
+    }
+    handleIncrement=() => {
+
+        this.setState({count: this.state.count+1});
+        console.log("Increment Clicked!",this.state.count);
     }
 }
  
